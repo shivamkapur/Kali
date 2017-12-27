85abd8b7e2cb24bd5267eb7fb75db5880cafb165
Shivam Kapur <shivam@heady.io>
2017-12-27 16:23:16 +0530

    Initial Swift 4 migration

---
+++ DerivedData/DateTime/Build/Intermediates.noindex/SwiftMigration/Kali/Intermediates.noindex/Pods.build/Debug-iphoneos/Kali.build/Objects-normal/arm64/Kali-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/shivamkapur/Desktop/Kali/Pods/Target\ Support\ Files/Kali/Kali-dummy.m \
+  /Users/shivamkapur/Desktop/Kali/DerivedData/DateTime/Build/Intermediates.noindex/SwiftMigration/Kali/Intermediates.noindex/Pods.build/Debug-iphoneos/Kali.build/module.modulemap \
+  /Users/shivamkapur/Desktop/Kali/Pods/Target\ Support\ Files/Kali/Kali-prefix.pch
